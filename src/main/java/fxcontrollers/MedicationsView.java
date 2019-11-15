edc3d4e0c73f1cd4d6905652489c12b8c5905978
ian.chelaru <ianchelaru@yahoo.com>
2019-11-15 11:29:35 +0200

    display medications depending on the parts of the day

--- src/main/java/fxcontrollers/MedicationsView.java
+++ src/main/java/fxcontrollers/MedicationsView.java
@@ -19,11 +19,22 @@ import static grpc.MedicationServiceOuterClass.*;
 
 public class MedicationsView
 {
-    private static final LocalTime changeTime = LocalTime.of(0,50,40);
-    private static final LocalTime startMorningTime = LocalTime.of(5,0,0);
-    private static final LocalTime startAfternoonTime = LocalTime.of(12,0,0);
-    private static final LocalTime startEveningTime = LocalTime.of(17,0,0);
-    private static final LocalTime startNightTime = LocalTime.of(21,0,0);
+    private static final LocalTime changeTime = LocalTime.of(10,34,0);
+    private static final LocalTime morningStartTime = LocalTime.of(5,0,0);
+    private static final LocalTime afternoonStartTime = LocalTime.of(12,0,0);
+    private static final LocalTime eveningStartTime = LocalTime.of(17,0,0);
+    private static final LocalTime nightStartTime = LocalTime.of(21,0,0);
+
+    private GRPCClient grpcClient;
+
+    private List<Label> morningLabelList = new ArrayList<>();
+    private List<Label> afternoonLabelList = new ArrayList<>();
+    private List<Label> eveningLabelList = new ArrayList<>();
+
+    private List<Button> morningButtonList = new ArrayList<>();
+    private List<Button> afternoonButtonList = new ArrayList<>();
+    private List<Button> eveningButtonList = new ArrayList<>();
+
 
     @FXML
     private VBox labelsBox;
@@ -34,8 +45,6 @@ public class MedicationsView
     @FXML
     private Label time;
 
-    private GRPCClient grpcClient;
-
     @FXML
     public void initialize()
     {
@@ -45,27 +54,47 @@ public class MedicationsView
 
         labelsBox.setSpacing(5);
         buttonsBox.setSpacing(5);
+
+        downloadMedicationPlan();
+        initDisplay();
     }
 
     private void downloadMedicationPlan()
     {
-        List<Button> buttonList = new ArrayList<>();
-        List<Label> labelList = new ArrayList<>();
-
         List<Medication> medications = grpcClient.getMedications();
 
         for (Medication medication : medications)
         {
-            Label label = createLabel(medication.getMedicationName());
-            labelList.add(label);
-            buttonList.add(createButton(label));
+            parseDailyInterval(medication);
         }
 
-        labelsBox.getChildren().clear();
-        labelsBox.getChildren().addAll(labelList);
+        System.out.println(morningLabelList);
+        System.out.println(afternoonLabelList);
+        System.out.println(eveningLabelList);
+    }
 
-        buttonsBox.getChildren().clear();
-        buttonsBox.getChildren().addAll(buttonList);
+    private void parseDailyInterval(Medication medication)
+    {
+        String dailyInterval = medication.getDailyInterval();
+        String[] intervals = dailyInterval.split("-");
+        if ("1".equals(intervals[0]))
+        {
+            Label label = createLabel(medication.getMedicationName());
+            morningLabelList.add(label);
+            morningButtonList.add(createButton(label));
+        }
+        if ("1".equals(intervals[1]))
+        {
+            Label label = createLabel(medication.getMedicationName());
+            afternoonLabelList.add(label);
+            afternoonButtonList.add(createButton(label));
+        }
+        if ("1".equals(intervals[2]))
+        {
+            Label label = createLabel(medication.getMedicationName());
+            eveningLabelList.add(label);
+            eveningButtonList.add(createButton(label));
+        }
     }
 
     private Label createLabel(String medicationName)
@@ -93,15 +122,73 @@ public class MedicationsView
         Timeline clock = new Timeline(new KeyFrame(Duration.ZERO,e ->
         {
             LocalTime currentTime = LocalTime.now();
-            if ((currentTime.getHour() == changeTime.getHour()) &&
-                    (currentTime.getMinute() == changeTime.getMinute()) &&
-                    (currentTime.getSecond() == changeTime.getSecond()))
+
+            if (areLocalTimesEqual(currentTime,changeTime))
             {
                 downloadMedicationPlan();
+                System.out.println("Medication plan downloaded");
             }
+
+            if (areLocalTimesEqual(currentTime,morningStartTime))
+            {
+                display(morningLabelList, morningButtonList);
+            }
+
+            if (areLocalTimesEqual(currentTime,afternoonStartTime))
+            {
+                display(afternoonLabelList, afternoonButtonList);
+            }
+
+            if (areLocalTimesEqual(currentTime, eveningStartTime))
+            {
+                display(eveningLabelList, eveningButtonList);
+            }
+
+            if (areLocalTimesEqual(currentTime, nightStartTime))
+            {
+                display(new ArrayList<>(), new ArrayList<>());
+            }
+
             time.setText(currentTime.getHour() + ":" + currentTime.getMinute() + ":" + currentTime.getSecond());
         }),new KeyFrame(Duration.seconds(1)));
         clock.setCycleCount(Animation.INDEFINITE);
         clock.play();
+    }
+
+    private boolean areLocalTimesEqual(LocalTime time1,LocalTime time2)
+    {
+        return (time1.getHour() == time2.getHour()) && (time1.getMinute() == time2.getMinute()) &&
+                (time1.getSecond() == time2.getSecond());
+    }
+
+    private void display(List<Label> labelList, List<Button> buttonList)
+    {
+        labelsBox.getChildren().clear();
+        labelsBox.getChildren().addAll(labelList);
+
+        buttonsBox.getChildren().clear();
+        buttonsBox.getChildren().addAll(buttonList);
+    }
+
+    private void initDisplay()
+    {
+        LocalTime currentTime = LocalTime.now();
+
+        if (currentTime.isAfter(morningStartTime) && currentTime.isBefore(afternoonStartTime))
+        {
+            display(morningLabelList, morningButtonList);
+        }
+        if (currentTime.isAfter(afternoonStartTime) && currentTime.isBefore(eveningStartTime))
+        {
+            display(afternoonLabelList, afternoonButtonList);
+        }
+        if (currentTime.isAfter(eveningStartTime) && currentTime.isBefore(nightStartTime))
+        {
+            display(eveningLabelList, eveningButtonList);
+        }
+        if (currentTime.isAfter(nightStartTime) && currentTime.isBefore(morningStartTime))
+        {
+            display(new ArrayList<>(), new ArrayList<>());
+        }
     }
 }
